2d78ec0f49d4d1c5c841c09893ca73ada1c59b62
Paul Stanley <paul.stanley@asos.com>
2018-07-09 10:50:48 +0100

    Added FuncTimer

---
+++ src/ByteDev.Common/Time/FuncTimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ByteDev.Common.Time
+{
+    public class FuncTimer
+    {
+        /// <summary>
+        /// Provided func will keep getting called until it returns true or the timeout is reached
+        /// (which ever occurs first).
+        /// </summary>
+        /// <returns>True, the func returned true in time. False, the func timed out.</returns>
+        public static bool WaitFuncReturnsTrueOrTimeout(Func<bool> func, TimeSpan timeout)
+        {
+            var source = new Stopwatch();
+            source.Start();
+
+            while (source.Elapsed < timeout)
+            {
+                if (func())
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
