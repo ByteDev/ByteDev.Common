2aa36fb53cf6051f4c083299998acffecb69dd41
bytedev <paulstanley@protonmail.com>
2019-11-25 19:24:21 +0800

    Added more xmldoc for public methods

--- src/ByteDev.Common/UriExtensions.cs
+++ src/ByteDev.Common/UriExtensions.cs
@@ -11,54 +11,76 @@ namespace ByteDev.Common
     /// </summary>
     public static class UriExtensions
     {
+        /// <summary>
+        /// Add a path to a Uri that does not have any path.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <param name="path">The path to add.</param>
+        /// <returns>A new Uri.</returns>
+        /// <exception cref="T:System.ArgumentException"><paramref name="path" /> was null or empty.</exception>
+        /// <exception cref="T:System.InvalidOperationException"><paramref name="source" /> already has a path or query.</exception>
         public static Uri AddPath(this Uri source, string path)
         {
             if (string.IsNullOrEmpty(path))
-            {
                 throw new ArgumentException("Path was null or empty.");
-            }
-
+            
             if (!source.HasNoPathAndQuery())
-            {
                 throw new InvalidOperationException("Source Uri already has a path or query.");                
-            }
+            
             return new Uri(source, path);
         }
 
+        /// <summary>
+        /// Returns a dictionary based on the Uri query string.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <returns>A dictionary of query string name value pairs.</returns>
         public static Dictionary<string, string> GetQueryAsDictionary(this Uri source)
         {
             if (!source.HasQuery())
-            {
                 return new Dictionary<string, string>();
-            }
+            
             return source.Query.Substring(1).Split('&').ToDictionary(p => p.Split('=')[0], p => p.Split('=')[1]);
         }
 
+        /// <summary>
+        /// Add or modify a query string parameter.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <param name="name">Query string parameter name.</param>
+        /// <param name="value">Query string parameter value.</param>
+        /// <returns>A Uri with the added or modified query string parameter.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="source" /> is null.</exception>
+        /// <exception cref="T:System.ArgumentException"><paramref name="name" /> was null or empty.</exception>
         public static Uri AddOrModifyQueryStringParam(this Uri source, string name, string value)
         {
             if (source == null)
                 throw new ArgumentNullException(nameof(source));
 
             if (string.IsNullOrEmpty(name))
-                throw new ArgumentException("Name was be null or empty.", nameof(name));
+                throw new ArgumentException("Name was null or empty.", nameof(name));
 
             var uriBuilder = new UriBuilder(source);
             var nameValues = HttpUtility.ParseQueryString(uriBuilder.Query);
 
             if (value == null)
-            {
                 nameValues.Remove(name);
-            }
             else
-            {
                 nameValues.AddOrUpdate(name, value);
-            }
 
             uriBuilder.Query = nameValues.ToString();
             
             return uriBuilder.Uri;
         }
 
+        /// <summary>
+        /// Add or modify a set of query string parameters.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <param name="obj">Object with a Name property and any other property that contains the value.</param>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="source" /> is null.</exception>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="obj" /> was null.</exception>
+        /// <returns>A Uri with the added or modified query string parameters.</returns>
         public static Uri AddOrModifyQueryStringParams(this Uri source, object obj)
         {
             if (source == null)
@@ -85,10 +107,19 @@ namespace ByteDev.Common
             return uri;
         }
 
+        /// <summary>
+        /// Remove query string parameter.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <param name="name">The name of the parameter to remove.</param>
+        /// <returns>Uri with any parameter with <paramref name="name" /> removed.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="source" /> is null.</exception>
+        /// <exception cref="T:System.ArgumentException"><paramref name="name" /> was null or empty.</exception>
         public static Uri RemoveQueryStringParam(this Uri source, string name)
         {
             if (source == null)
                 throw new ArgumentNullException(nameof(source));
+
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("Name was null or empty.", nameof(name));
 
@@ -102,6 +133,12 @@ namespace ByteDev.Common
             return uriBuilder.Uri;
         }
 
+        /// <summary>
+        /// Removes any query string from the Uri.
+        /// </summary>
+        /// <param name="source">The Uri to perform the operation on.</param>
+        /// <returns>Uri with no query string.</returns>
+        /// <exception cref="T:System.ArgumentNullException"><paramref name="source" /> is null.</exception>
         public static Uri ClearQueryString(this Uri source)
         {
             if (source == null)
@@ -111,16 +148,31 @@ namespace ByteDev.Common
             return uriBuilder.Uri;
         }
 
+        /// <summary>
+        /// Indicates whether the Uri has no path and query string.
+        /// </summary>
+        /// <param name="source">The uri to perform the operation on.</param>
+        /// <returns>True if <paramref name="source" /> has no path and no query string; otherwise returns false.</returns>
         public static bool HasNoPathAndQuery(this Uri source)
         {
-            return string.IsNullOrEmpty(source.PathAndQuery) || (source.PathAndQuery == "/");
+            return string.IsNullOrEmpty(source.PathAndQuery) || source.PathAndQuery == "/";
         }
 
+        /// <summary>
+        /// Indicates whether the Uri has any query string.
+        /// </summary>
+        /// <param name="source">The uri to perform the operation on.</param>
+        /// <returns>True if <paramref name="source" /> has a query string; otherwise returns false.</returns>
         public static bool HasQuery(this Uri source)
         {
             return !string.IsNullOrEmpty(source.Query) && (source.Query != "?");
         }
 
+        /// <summary>
+        /// Indicates whether the Uri has any fragment.
+        /// </summary>
+        /// <param name="source">The uri to perform the operation on.</param>
+        /// <returns>True if <paramref name="source" /> has a fragment; otherwise returns false.</returns>
         public static bool HasFragment(this Uri source)
         {
             return !string.IsNullOrEmpty(source.Fragment) && (source.Fragment != "#");
