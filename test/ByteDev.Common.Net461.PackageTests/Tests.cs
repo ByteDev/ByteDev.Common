a7c5617765fe81c61b1a5c02c0f31736c768be72
Paul Stanley <paulstanley@protonmail.com>
2020-02-19 14:04:09 +0800

    Added PackageTest projects, solution and build files

---
+++ test/ByteDev.Common.Net461.PackageTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ByteDev.Common.Net461.PackageTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void WhenStartsWithValue_ThenRemoveStartingValue()
+        {
+            var result = "My name is John".RemoveStartsWith("My");
+
+            Assert.That(result, Is.EqualTo(" name is John"));
+        }
+    }
+}
---
+++ test/ByteDev.Common.NetCore.PackageTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ByteDev.Common.NetCore.PackageTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void WhenStartsWithValue_ThenRemoveStartingValue()
+        {
+            var result = "My name is John".RemoveStartsWith("My");
+
+            Assert.That(result, Is.EqualTo(" name is John"));
+        }
+    }
+}
