6829ada814da8a43e24af9fc67406d2705873ef7
Paul Stanley <paulstanley@protonmail.com>
2020-05-07 18:40:06 +0800

    Deleted reflection extension methods

--- src/ByteDev.Common/GenericExtensions.cs
+++ src/ByteDev.Common/GenericExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
@@ -12,30 +11,6 @@ namespace ByteDev.Common
     /// </summary>
     public static class GenericExtensions
     {
-        /// <summary>
-        /// Invokes a method on a type using reflection.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the object to invoke the method on.</typeparam>
-        /// <param name="source">The object to invoke the method on.</param>
-        /// <param name="methodName">The name of the method to invoke.</param>
-        /// <param name="args">Any arguments to pass to the invoked method.</param>
-        /// <returns>The value returned from the invoked method. If the return type is void then null will be returned.</returns>
-        /// <exception cref="T:System.ArgumentNullException"><paramref name="source" /> is null.</exception>
-        /// <exception cref="T:System.ArgumentException"><paramref name="methodName" /> does not exist.</exception>
-        public static object InvokeMethod<TSource>(this TSource source, string methodName, params object[] args)
-        {
-            if(source == null)
-                throw new ArgumentNullException(nameof(source));
-
-            var type = typeof(TSource);
-            var method = type.GetTypeInfo().GetDeclaredMethod(methodName);
-
-            if (method == null)
-                throw new ArgumentException($"Method '{methodName}' does not exist.", nameof(methodName));
-
-            return method.Invoke(source, args);
-        }
-        
         /// <summary>
         /// Checks whether a value is contained in a list of values.
         /// </summary>
--- test/ByteDev.Common.UnitTests/GenericExtensionsTest.cs
+++ test/ByteDev.Common.UnitTests/GenericExtensionsTest.cs
@@ -6,30 +6,6 @@ namespace ByteDev.Common.UnitTests
     [TestFixture]
     public class GenericExtensionsTest
     {
-        protected internal bool MyProtectedInternalMethod()
-        {
-            return true;
-        }
-
-        internal bool MyInternalMethod()
-        {
-            return true;
-        }
-
-        protected bool MyProtectedMethod()
-        {
-            return true;
-        }
-
-        private bool MyPrivateMethod()
-        {
-            return true;
-        }
-
-        private void MyPrivateMethodReturnVoid()
-        {
-        }
-
         [TestFixture]
         public class CloneSerializable : GenericExtensionsTest
         {
@@ -85,103 +61,6 @@ namespace ByteDev.Common.UnitTests
             public class DummySerializableComplex
             {
                 public DummySerializable InnerDummy { get; set; }
-            }
-        }
-
-        [TestFixture]
-        public class InvokeMethod : GenericExtensionsTest
-        {
-            [Test]
-            public void WhenSourceIsNull_ThenThrowException()
-            {
-                Assert.Throws<ArgumentNullException>(() => GenericExtensions.InvokeMethod(null as object, "MyProtectedInternalMethod"));
-            }
-
-            [Test]
-            public void WhenMethodNameIsNull_ThenThrowException()
-            {
-                var sut = CreateSut();
-
-                Assert.Throws<ArgumentNullException>(() => sut.InvokeMethod(null));
-            }
-
-            [Test]
-            public void WhenMethodNameIsEmpty_ThenThrowException()
-            {
-                var sut = CreateSut();
-
-                Assert.Throws<ArgumentException>(() => sut.InvokeMethod(string.Empty));
-            }
-
-            [Test]
-            public void WhenMethodNameDoesNotExist_ThenThrowException()
-            {
-                var sut = CreateSut();
-
-                Assert.Throws<ArgumentException>(() => sut.InvokeMethod("ThisMethodDoesNotExist"));
-            }
-
-            [Test]
-            public void WhenInvokingExistingProtectedInternalMethod_ThenReturnValue()
-            {
-                var sut = CreateSut();
-
-                var result = sut.InvokeMethod("MyProtectedInternalMethod");
-
-                Assert.That(result, Is.True);
-            }
-
-            [Test]
-            public void WhenInvokingExistingInternalMethod_ThenReturnValue()
-            {
-                var sut = CreateSut();
-
-                var result = sut.InvokeMethod("MyInternalMethod");
-
-                Assert.That(result, Is.True);
-            }
-
-            [Test]
-            public void WhenInvokingExistingProtectedMethod_ThenReturnValue()
-            {
-                var sut = CreateSut();
-
-                var result = sut.InvokeMethod("MyProtectedMethod");
-
-                Assert.That(result, Is.True);
-            }
-
-            [Test]
-            public void WhenInvokingExistingPrivateMethod_ThenReturnValue()
-            {
-                var sut = CreateSut();
-
-                var result = sut.InvokeMethod("MyPrivateMethod");
-
-                Assert.That(result, Is.True);
-            }
-
-            [Test]
-            public void WhenInvokingExistingMethodWithVoidReturn_ThenReturnNull()
-            {
-                var sut = CreateSut();
-
-                var result = sut.InvokeMethod("MyPrivateMethodReturnVoid");
-
-                Assert.That(result, Is.Null);
-            }
-
-            [Test]
-            public void WhenInvokingNonExistingMethod_ThenThrowException()
-            {
-                var sut = CreateSut();
-
-                Assert.Throws<ArgumentException>(() => sut.InvokeMethod("MyInternalMethod1"));
-            }
-
-            private static GenericExtensionsTest CreateSut()
-            {
-                return new GenericExtensionsTest();
             }
         }
 
